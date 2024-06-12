1c7b3deec2ed11b4313b429433cf40232d1a5d8b
Tarkeshwar Kumar <tarkeshwar.kumar@hp.com>
2024-06-12 10:38:59 +0530

    Removed explicit db connection

--- src/server/auth/auth.py
+++ src/server/auth/auth.py
@@ -1,11 +1,5 @@
 import mysql.connector as sql
 
-conn = sql.connect(
-  host="localhost",
-  user="root",
-  password="chelsiGoyal@123",
-  database="foodApp"
-)
 
 class Auth:
     def authenticate(user_id: str, password: str):
